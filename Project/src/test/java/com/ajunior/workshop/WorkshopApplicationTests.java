af4a7816321356ea25f66277d105fa7d25e75b2d
Amauri Junior <apojr98@gmail.com>
2022-07-27 19:47:33 -0300

    First commit

---
+++ Project/src/test/java/com/ajunior/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajunior.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
