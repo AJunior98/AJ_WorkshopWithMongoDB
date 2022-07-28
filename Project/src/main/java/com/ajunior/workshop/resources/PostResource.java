d3702a6ae680b6c084b5874f32e150dc8c2c6f6c
Amauri Junior <apojr98@gmail.com>
2022-07-28 17:38:00 -0300

    Simple query with query methods

--- Project/src/main/java/com/ajunior/workshop/resources/PostResource.java
+++ Project/src/main/java/com/ajunior/workshop/resources/PostResource.java
@@ -1,13 +1,18 @@
 package com.ajunior.workshop.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ajunior.workshop.dto.PostDTO;
+import com.ajunior.workshop.entities.Post;
+import com.ajunior.workshop.resources.util.URL;
 import com.ajunior.workshop.services.PostService;
 
 @RestController
@@ -21,5 +26,12 @@ public class PostResource {
 	public ResponseEntity<PostDTO> findById(@PathVariable String id){
 		PostDTO dto = postService.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
--- Project/src/main/java/com/ajunior/workshop/services/PostService.java
+++ Project/src/main/java/com/ajunior/workshop/services/PostService.java
@@ -1,5 +1,6 @@
 package com.ajunior.workshop.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,4 +23,7 @@ public class PostService {
 		return new PostDTO(entity);
 	}
 	
+	public List<Post> findByTitle(String text){
+		return postRepository.findByTitleContainingIgnoreCase(text);
+	}
 }
